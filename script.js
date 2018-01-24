336a72bb69eccdcd256166cc01f8cbc821e5dfee
Andrej <andrej.byra@centrum.sk>
2018-01-24 15:42:39 +0100

    fixed script and html file

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 $(document).ready(function(){
 	$("#get").click(function(){
-		var city=$("#city").val();
-		var codes=$("#code").val();
+		var city=$("#entercity").val();
+		var code=$("#entercode").val();
 		if (city.length>1) {
 			var urllink='https://api.openweathermap.org/data/2.5/weather?q=';
 			urllink=urllink + city;
-			if (code.length==2)
+			if (code.length==2){
 				urllink=urllink+','+code;
-			urllink=urllink+','+code;
+			}
 			urllink=urllink+'&APPID=f7fabd71df106f511e278922bc54e16a';
 
 			$.ajax(
 			{
-				url: urlLink,
+				url: urllink,
 				data: {format: 'json'},
 				error: function()
 				{
